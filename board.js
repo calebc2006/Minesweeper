f45c59576cb417ae42da58f63e476154b3294275
caleb <77676501+calebc2006@users.noreply.github.com>
2022-02-23 11:21:53 +0800

    Add files via upload

---
+++ board.js
@@ -0,0 +1,65 @@
+export const BOARD_COLUMNS = 9
+export const BOARD_ROWS = 9
+export const NUM_BOMBS = Math.floor((BOARD_COLUMNS * BOARD_ROWS) / 5)
+
+const dir = [
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [-1, 0],
+]
+
+let visited = generateBlankBoard()
+
+function dfs(row, col) {
+  if (visited[row][col] === 1) return
+  visited[row][col] = 1
+
+  dir.forEach(([x, y]) => {
+    let [newRow, newCol] = [row + x, col + y]
+    if (
+      newRow < 0 ||
+      newRow >= BOARD_ROWS ||
+      newCol < 0 ||
+      newCol >= BOARD_COLUMNS
+    ) {
+      return
+    }
+
+    dfs(newRow, newCol)
+  })
+}
+
+export function generateBlankBoard() {
+  const blankBoard = []
+
+  for (let i = 0; i < BOARD_ROWS; i++) {
+    let curRow = []
+    for (let j = 0; j < BOARD_ROWS; j++) {
+      curRow.push(0)
+    }
+    blankBoard.push(curRow)
+  }
+  return blankBoard
+}
+
+export function generateRandomBoard() {
+  let board = generateBlankBoard()
+  let bombCoords = []
+
+  while (bombCoords.length < NUM_BOMBS) {
+    let newBomb = [randomNumBelow(BOARD_ROWS), randomNumBelow(BOARD_COLUMNS)]
+    if (!bombCoords.includes(newBomb)) {
+      bombCoords.push(newBomb)
+    }
+  }
+
+  for (let i = 0; i < NUM_BOMBS; i++) {
+    board[bombCoords[i][0]][bombCoords[i][1]] = 1
+  }
+  return [board, bombCoords]
+}
+
+function randomNumBelow(x) {
+  return Math.floor(Math.random() * x)
+}
---
+++ script.js
@@ -0,0 +1,286 @@
+import {
+  generateRandomBoard,
+  NUM_BOMBS,
+  BOARD_COLUMNS,
+  BOARD_ROWS,
+  generateBlankBoard,
+} from "./board.js"
+
+const gameBoardElem = document.getElementById("game-board")
+const resetBtn = document.getElementById("reset-btn")
+const header = document.getElementById("game-over")
+const timerElem = document.getElementById("timer")
+let GAMEOVER = false
+let USER_WIN = false
+let START_TIME = 0
+let curBoard = generateRandomBoard()[0]
+let bombCoords = generateRandomBoard()[1]
+
+function gameLoop() {
+  fillBoard()
+  showStartTiles()
+  startGame()
+  START_TIME = Date.now()
+
+  setInterval(() => {
+    checkWin()
+    if (GAMEOVER) {
+      endGame()
+      return
+    } else timerElem.innerHTML = formatTime(Date.now() - START_TIME)
+  }, 50)
+}
+
+resetBtn.addEventListener("click", () => {
+  gameLoop()
+  GAMEOVER = false
+})
+
+function fillBoard() {
+  gameBoardElem.innerHTML = ""
+  for (let row = 0; row < BOARD_ROWS; row++) {
+    for (let col = 0; col < BOARD_COLUMNS; col++) {
+      let tileElem = document.createElement("button")
+      tileElem.classList.add("board-tile", "unselected")
+
+      if (curBoard[row][col] === 1) {
+        let icon = document.createElement("i")
+        tileElem.appendChild(icon)
+        tileElem.classList.add("flag")
+        tileElem.setAttribute("flag", 1)
+      } else {
+        tileElem.setAttribute("numFlagsAround", numFlagsAround(row, col))
+      }
+
+      gameBoardElem.appendChild(tileElem)
+    }
+  }
+}
+
+function showStartTiles() {
+  let startTiles = getRandomStart()
+  startTiles.forEach((tile) => {
+    showTile(tile)
+  })
+}
+
+function getTileElement(row, col) {
+  const children = gameBoardElem.children
+  let index = row * BOARD_COLUMNS + col
+  return children[index]
+}
+
+function startGame() {
+  gameBoardElem.addEventListener("click", onClick)
+  gameBoardElem.addEventListener("contextmenu", onRightClick)
+  header.innerHTML = "MINESWEEPER"
+  resetBtn.innerHTML = "RESET"
+}
+
+function onClick(e) {
+  let tileElemSelected = e.target
+
+  if (tileElemSelected.nodeName === "I") {
+    tileElemSelected = tileElemSelected.parentElement
+  }
+  if (
+    tileElemSelected.nodeName === "DIV" ||
+    tileElemSelected.classList.contains("marked")
+  ) {
+    return
+  }
+
+  let num = tileElemSelected.getAttribute("numFlagsAround")
+  if (num === "0") {
+    visited = generateBlankBoard()
+    tilesReached = [tileElemSelected]
+    let index = 0
+    while (gameBoardElem.children[index] !== tileElemSelected) {
+      index++
+    }
+    let col = index % BOARD_COLUMNS
+    let row = (index - col) / BOARD_COLUMNS
+    dfs(row, col, true)
+    tilesReached.forEach((tile) => {
+      showTile(tile)
+    })
+  }
+
+  tileElemSelected.classList.remove("unselected")
+  showTile(tileElemSelected)
+}
+
+function onRightClick(e) {
+  e.preventDefault()
+  let tileElemSelected = e.target
+  if (tileElemSelected.nodeName === "DIV") {
+    return
+  }
+  if (tileElemSelected.nodeName === "I") {
+    tileElemSelected = tileElemSelected.parentElement
+  }
+  if (tileElemSelected.classList.contains("number")) {
+    return
+  }
+
+  tileElemSelected.classList.remove("unselected")
+  if (tileElemSelected.classList.contains("marked")) {
+    tileElemSelected.classList.remove("marked", "fa-solid", "fa-flag")
+    tileElemSelected.classList.add("unselected")
+    if (tileElemSelected.classList.contains("number")) {
+      tileElemSelected.classList.remove("number")
+    }
+  } else {
+    tileElemSelected.classList.add("marked", "fa-solid", "fa-flag")
+  }
+}
+
+function endGame() {
+  gameBoardElem.removeEventListener("click", onClick)
+  if (!USER_WIN) header.innerHTML = "GAME OVER!"
+  else header.innerHTML = "PUZZLE COMPLETED! (refresh for new puzzle)"
+  resetBtn.innerHTML = "TRY AGAIN"
+  showAnswer()
+}
+
+function numFlagsAround(row, col) {
+  let directions = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ]
+  let numFlags = 0
+
+  directions.forEach((i) => {
+    let dx = i[0]
+    let dy = i[1]
+    if (
+      row + dx < 0 ||
+      row + dx >= BOARD_ROWS ||
+      col + dy < 0 ||
+      col + dy >= BOARD_COLUMNS
+    ) {
+      numFlags = numFlags
+    } else if (curBoard[row + dx][col + dy] === 1) {
+      numFlags++
+    }
+  })
+
+  return numFlags
+}
+
+function showAnswer() {
+  let childList = gameBoardElem.children
+  for (let i = 0; i < BOARD_COLUMNS * BOARD_ROWS; i++) {
+    let tileElemSelected = childList[i]
+    if (tileElemSelected.classList.contains("unselected")) {
+      tileElemSelected.classList.remove("unselected")
+    }
+    showTile(tileElemSelected)
+  }
+}
+
+function showTile(tileElemSelected) {
+  if (tileElemSelected.classList.contains("marked")) {
+    tileElemSelected.classList.remove("marked", "fa-solid", "fa-flag")
+  }
+
+  if (tileElemSelected.hasAttribute("flag")) {
+    tileElemSelected.firstChild.classList.add("fa-solid", "fa-bomb")
+    GAMEOVER = true
+  } else {
+    let num = tileElemSelected.getAttribute("numFlagsAround")
+    if (num === "0") num = ""
+    tileElemSelected.innerHTML = num
+    tileElemSelected.classList.add("number")
+    if (tileElemSelected.classList.contains("unselected")) {
+      tileElemSelected.classList.remove("unselected")
+    }
+  }
+}
+
+function formatTime(time) {
+  time = Math.floor(time / 1000)
+  let secs = time % 60
+  if (secs < 10) {
+    secs = "0" + secs.toString()
+  }
+  let mins = (time - secs) / 60
+  mins = mins.toString()
+  return mins + ":" + secs
+}
+
+function getRandomStart() {
+  let blankSquares = []
+  for (let row = 0; row < BOARD_ROWS; row++) {
+    for (let col = 0; col < BOARD_COLUMNS; col++) {
+      if (curBoard[row][col] === 0 && numFlagsAround(row, col) === 0) {
+        blankSquares.push([row, col])
+      }
+    }
+  }
+  let startTile = blankSquares[Math.floor(blankSquares.length / 2)]
+  tilesReached = [getTileElement(startTile[0], startTile[1])]
+  visited = generateBlankBoard()
+  dfs(startTile[0], startTile[1], true)
+
+  return tilesReached
+}
+
+let visited = generateBlankBoard()
+let tilesReached = []
+const dir = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+]
+
+function dfs(row, col, first) {
+  if (first) {
+    first = false
+    visited = generateBlankBoard()
+  }
+  if (visited[row][col] === 1) return
+  if (curBoard[row][col] === 1) return
+  visited[row][col] = 1
+  tilesReached.push(getTileElement(row, col))
+  if (numFlagsAround(row, col) > 0) return
+
+  dir.forEach(([x, y]) => {
+    let [newRow, newCol] = [row + x, col + y]
+    if (
+      newRow < 0 ||
+      newRow >= BOARD_ROWS ||
+      newCol < 0 ||
+      newCol >= BOARD_COLUMNS
+    ) {
+      return
+    }
+
+    dfs(newRow, newCol, false)
+  })
+}
+
+function checkWin() {
+  if (GAMEOVER) return
+  for (let row = 0; row < BOARD_ROWS; row++) {
+    for (let col = 0; col < BOARD_COLUMNS; col++) {
+      if (bombCoords.includes([row, col])) continue
+      if (getTileElement(row, col).classList.contains("flag")) continue
+      if (getTileElement(row, col).classList.contains("unselected"))
+        return false
+    }
+  }
+  GAMEOVER = true
+  USER_WIN = true
+}
